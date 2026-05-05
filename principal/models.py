2aeac5b08fa2b81510bbba8c64cacaf725dff6ba
NikhilDendeti <nikhildendeti@gmail.com>
2026-05-05 15:56:23 +0530

    models changes-2

--- principal/models.py
+++ principal/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class PrincipalProfile(models.Model):
     user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
+        'core.User',
         on_delete=models.CASCADE,
     )
     school = models.ForeignKey(
