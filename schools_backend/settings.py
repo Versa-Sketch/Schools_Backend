fa9dbca3e042a8661ed6b181f09ddbe95f5296f2
NikhilDendeti <dendetinikhil@gmail.com>
2026-05-10 12:34:27 +0530

    feat: initialize Django project configuration and dependencies

--- schools_backend/settings.py
+++ schools_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-io26ghb+331r%f!1v5wz9v)fs&(-vq%3+(5vy3puif!qa#e)bi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -134,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
